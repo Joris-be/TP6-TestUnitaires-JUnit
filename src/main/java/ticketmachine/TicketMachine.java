b70231bb4d748b176bd0a7680dc907c699e843a6
Joris-be <joris.beldjoudi@gmail.com>
2025-11-01 18:32:15 +0100

    TP6 VERSION FINALE

--- src/main/java/ticketmachine/TicketMachine.java
+++ src/main/java/ticketmachine/TicketMachine.java
@@ -63,6 +63,9 @@ public class TicketMachine {
 	 * @throws IllegalArgumentException if amount is not positive
 	 */
 	public void insertMoney(int amount) {
+		if (amount <= 0) {
+    throw new IllegalArgumentException("Amount must be positive");
+	}
 		balance = balance + amount;
 	}
 
@@ -72,8 +75,10 @@ public class TicketMachine {
 	 * @return the balance
 	 */
 	public int refund() {
-		System.out.println("Je vous rends : " + balance + " centimes");
-		return balance;
+		int refunded = balance;
+		balance = 0;
+		return refunded;
+
 	}
 
 	/**
@@ -82,6 +87,11 @@ public class TicketMachine {
 	 * @return vrai si le ticket a été imprimé, faux sinon
 	 */
 	public boolean printTicket() {
+		if (balance < price) {
+        return false;
+    }
+		balance -= price;
+		total += price;
 		// Simulate the printing of a ticket.
 		System.out.println("##################");
 		System.out.println("# The BlueJ Line");
--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -1,7 +1,11 @@
 package ticketmachine;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class TicketMachineTest {
 	private static final int PRICE = 50; // Une constante
@@ -33,4 +37,70 @@ class TicketMachineTest {
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");
 	}
 
+	@Test
+	// S3 : on n'imprime pas le ticket si le montant inséré est insuffisant
+	void cannotPrintTicketWhenBalanceIsInsufficient() {
+		machine.insertMoney(PRICE - 10);
+		assertFalse(machine.printTicket(), "Le ticket a été imprimé alors que la balance est insuffisante");	
+	}
+
+	@Test
+	// S4 : on imprime le ticket si le montant inséré est suffisant
+	void canPrintTicketWhenBalanceIsSufficient() {
+		machine.insertMoney(PRICE);
+		assertTrue(machine.printTicket(), "Le ticket n'a pas été imprimé alors que la balance est suffisante");
+	}
+
+	@Test
+	// S5 : Quand on imprime un ticket la balance est decrémentée du prix du ticket
+	void printingTicketDecreasesBalance() {
+		machine.insertMoney(PRICE + 20); // On insère plus que le prix du ticket
+		machine.printTicket(); // On imprime un ticket
+		assertEquals(20, machine.getBalance(), "La balance n'est pas correctement décrémentée après impression du ticket");
+	}
+
+	@Test
+	// S6 : Le montant collecté est mis à jour quand on imprime un ticket (pas avant)
+	void printingTicketIncreasesTotal() {
+        machine.insertMoney(PRICE);
+		assertEquals(0, machine.getTotal(),"Le total ne doit pas être mis à jour avant l'impression du ticket");
+        machine.printTicket();
+        assertEquals(PRICE, machine.getTotal(),"Le total doit être mis à jour après l'impression du ticket");
+	}
+	
+	@Test
+	// S7 : refund() rendcorrectement la monnaie
+	void refundReturnsBalance() {
+		machine.insertMoney(70);
+		int refund = machine.refund();
+		assertEquals(70, refund, "refund() ne rend pas correctement la monnaie");
+	}
+
+	@Test
+	// S8 : refund() remet la balance à zéro
+	void refundResetsBalance() {
+		machine.insertMoney(70);
+		machine.refund();
+		assertEquals(0, machine.getBalance(), "refund() ne remet pas la balance à zéro");
+	}
+
+	@Test
+	// S9 : on ne peut pas insérerun montant négatif
+	void cannotInsertNegativeAmount() {
+	assertThrows(IllegalArgumentException.class, () -> {
+        machine.insertMoney(-10);
+		}, "Insérer un montant négatif doit lever une exception");
+	}	
+
+	@Test
+	// S10 : on ne peut pas créer de machine qui délivre des tickets dont le prix est négatif
+	void cannotCreateMachineWithNegativeTicketPrice() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			new TicketMachine(-50);
+		}, "Créer une machine avec un prix négatif doit lever une exception");
+	}
+
+
 }
+
+
